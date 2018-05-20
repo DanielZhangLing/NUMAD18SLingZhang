4305545f98a7c7cb5fbd1b7e606cce88eb5cb4a3
Daniel Zhang <zhang.ling1@husky.neu.edu>
2018-05-20 10:04:44 -0400

    Tweak Comments

--- app/src/main/java/neu/edu/madcourse/lingzhang/DisplayAboutActivity.java
+++ app/src/main/java/neu/edu/madcourse/lingzhang/DisplayAboutActivity.java
@@ -1,6 +1,5 @@
 package neu.edu.madcourse.lingzhang;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,14 +10,13 @@ public class DisplayAboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_about);
-
-        Intent intent = getIntent();
         String message = "" +
                 "Name: Ling Zhang\n" +
                 "Email: zhang.ling1@husky.neu.edu\n" +
                 "Year: 2nd\n" +
                 "MEID: 99000736312328\n";
         TextView textView = findViewById(R.id.textView);
+        /* Set Message to Text View */
         textView.setText(message);
     }
 }
